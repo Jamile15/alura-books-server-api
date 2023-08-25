4e168b2c17fde0cd45da4f893826577fd89ec2ec
Jamile15 <112087958+Jamile15@users.noreply.github.com>
2023-08-25 15:39:33 -0300

    inserindo verificação de nome

--- controladores/livro.js
+++ controladores/livro.js
@@ -37,9 +37,16 @@ function getLivro(req, res) {
 function postLivro(req, res) {
     try {
         const livroNovo = req.body
-        insereLivro(livroNovo)
-        res.status(201)
-        res.send("livro inserido com sucesso")
+        if(req.body.nome){
+            insereLivro(livroNovo)
+            res.status(201)
+            res.send("livro inserido com sucesso")
+        }
+        else{
+            res.status(422)
+            res.send("CAmpo nome é obrigatorio")
+        }
+       
 
     } catch (error) {
         res.status(500)
